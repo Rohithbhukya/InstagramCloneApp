e3dfa3bb2dabda7af36e5980dd947837726ce466
Rohithbhukya <bhukyarohith491@gmail.com>
2021-04-20 12:15:59 +0530

    Instagram clone done

--- app/src/main/java/com/example/instagramcloneapp/LoginActivity.java
+++ app/src/main/java/com/example/instagramcloneapp/LoginActivity.java
@@ -1,18 +1,15 @@
 package com.example.instagramcloneapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.parse.LogInCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.shashank.sony.fancytoastlib.FancyToast;
@@ -20,7 +17,7 @@ import com.shashank.sony.fancytoastlib.FancyToast;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText edtLoginEmail, edtLoginPassword;
-    private Button btnLoginActivity, btnSignUpActivity;
+    private Button btnLoginActivity, btnSignUpLoginActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +28,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         edtLoginEmail = findViewById(R.id.edtLoginEmail);
         edtLoginPassword = findViewById(R.id.edtLoginPassword);
-
-        edtLoginPassword.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-
-                if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
-
-                    onClick(btnSignUpActivity);
-                }
-                return false;
-            }
-        });
-
         btnLoginActivity = findViewById(R.id.btnLoginActivity);
-        btnSignUpActivity = findViewById(R.id.btnSignUpLoginActivity);
+        btnSignUpLoginActivity = findViewById(R.id.btnSignUpLoginActivity);
 
         btnLoginActivity.setOnClickListener(this);
-        btnSignUpActivity.setOnClickListener(this);
+        btnSignUpLoginActivity.setOnClickListener(this);
 
-        if(ParseUser.getCurrentUser() != null){
+        if (ParseUser.getCurrentUser() != null) {
+
             ParseUser.getCurrentUser().logOut();
         }
 
@@ -62,49 +47,41 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (view.getId()) {
 
             case R.id.btnLoginActivity:
+                ParseUser.logInInBackground(edtLoginEmail.getText().toString(),
+                        edtLoginPassword.getText().toString(),
+                        new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException e) {
 
-                if (edtLoginEmail.getText().toString().equals("") || edtLoginPassword.getText().toString().equals("")) {
+                                if (user != null && e == null) {
 
-                    FancyToast.makeText(LoginActivity.this, "Email, Password is required",
-                            Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
-                    trasitionToSocialMediaActivity();
-
-                } else {
-                    ParseUser.logInInBackground(edtLoginEmail.getText().toString(), edtLoginPassword.getText().toString(), new LogInCallback() {
-                        @Override
-                        public void done(ParseUser user, ParseException e) {
-
-                            if (user != null && e == null) {
-
-                                FancyToast.makeText(LoginActivity.this, user.getUsername() + " is logged in succcessfully",
-                                        Toast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+                                    FancyToast.makeText(LoginActivity.this,
+                                            user.getUsername() + " is Logged in successfully",
+                                            Toast.LENGTH_SHORT, FancyToast.SUCCESS,
+                                            true).show();
+                                    transitionToSocialMediaActivity();
+                                }
                             }
-                        }
-                    });
+                        });
 
-                }
                 break;
+
             case R.id.btnSignUpLoginActivity:
+                Intent intent = new Intent(LoginActivity.this, SignUp.class);
+                startActivity(intent);
                 break;
 
-        }
 
+        }
     }
 
-    public void rootLayoutTapped(View view) {
 
-        try {
-            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
-    private void trasitionToSocialMediaActivity(){
+    private void transitionToSocialMediaActivity() {
 
         Intent intent = new Intent(LoginActivity.this, SocialMediaActivity.class);
         startActivity(intent);
+        finish();
     }
+
+
 }
--- app/src/main/java/com/example/instagramcloneapp/UsersTab.java
+++ app/src/main/java/com/example/instagramcloneapp/UsersTab.java
@@ -1,64 +1,140 @@
 package com.example.instagramcloneapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UsersTab#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class UsersTab extends Fragment {
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import java.util.ArrayList;
+import java.util.List;
+
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
+
+
+
+public class UsersTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+
+
+    private ListView listView;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter arrayAdapter;
 
     public UsersTab() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsersTab.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UsersTab newInstance(String param1, String param2) {
-        UsersTab fragment = new UsersTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setOnItemClickListener(UsersTab.this);
+        listView.setOnItemLongClickListener(UsersTab.this);
+
+        final TextView txtLoadingUsers = view.findViewById(R.id.txtLoadingUsers);
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e == null) {
+
+                    if (users.size() > 0) {
+
+                        for (ParseUser user : users) {
+
+                            arrayList.add(user.getUsername());
+
+                        }
+
+                        listView.setAdapter(arrayAdapter);
+                        txtLoadingUsers.animate().alpha(0).setDuration(2000);
+                        listView.setVisibility(View.VISIBLE);
+
+                    }
+
+                }
+            }
+        });
+        return view;
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Intent intent = new Intent(getContext(), UsersPosts.class);
+        intent.putExtra("username", arrayList.get(position));
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", arrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                if (user != null && e == null) {
+
+//                    FancyToast.makeText(getContext(), user.get("profileProfession") + "",
+//                            Toast.LENGTH_SHORT, FancyToast.SUCCESS,
+//                            true).show();
+                    final PrettyDialog prettyDialog =  new PrettyDialog(getContext());
+
+                    prettyDialog.setTitle(user.getUsername() + " 's Info")
+                            .setMessage(user.get("profileBio") + "\n"
+                                    + user.get("profileProfession") + "\n"
+                                    + user.get("profileHobbies") + "\n"
+                                    + user.get("profileFavSport"))
+                            .setIcon(R.drawable.person)
+                            .addButton(
+                                    "OK",     // button text
+                                    R.color.pdlg_color_white,  // button text color
+                                    R.color.pdlg_color_green,  // button background color
+                                    new PrettyDialogCallback() {  // button OnClick listener
+                                        @Override
+                                        public void onClick() {
+                                            // Do what you gotta do
+                                            prettyDialog.dismiss();
+                                        }
+                                    }
+                            )
+                            .show();
+
+
+                }
+            }
+        });
+
+
+        return true;
     }
 }
